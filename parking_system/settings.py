afaca5e7ba99e4c978cb16c546966a1ccf42ca3e
siddiquijamal <siddiquijamal654@gmail.com>
2026-01-07 15:19:40 +0530

    add

--- parking_system/settings.py
+++ parking_system/settings.py
@@ -146,7 +146,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 
 RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID")
